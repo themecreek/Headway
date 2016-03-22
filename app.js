c14789015a925a16b544f80fae58b50d9dc26e84
Zack Bloom <zackbloom@gmail.com>
2016-03-21 21:45:52 -0400

    Formatting and loading fixes

--- app.js
+++ app.js
@@ -1,59 +1,65 @@
 (function() {
+    if (!window.addEventListener)
+        return;
+
     var options = INSTALL_OPTIONS;
     style = document.createElement('style');
     document.head.appendChild(style);
     style.innerHTML = (
-      '#HW_badge_cont.HW_visible {' +
-      'display: inline-block !important;' +
-      '}'
-    ); 
-    render = function() {     
-        if (options.translate) {        
-        window.HW_config = {
-            account: options.id,
-            selector: options.location,
-            translations: {
-                title: options.title, 
-                labels: {
-                    "new": options.labelNew,
-                    "improvements": options.labelImprovement,
-                    "fix": options.labelFix
+        '#HW_badge_cont.HW_visible {' +
+            'display: inline-block !important;' +
+        '}'
+    );
+    var el;
+    render = function() {
+        el = Eager.createElement(options.location, el);
+        el.className = 'eager-headway-app';
+
+        if (options.translate) {
+            window.HW_config = {
+                account: options.id,
+                selector: '.eager-headway-app',
+                translations: {
+                    title: options.title,
+                    labels: {
+                        "new": options.labelNew,
+                        "improvements": options.labelImprovement,
+                        "fix": options.labelFix
+                    }
                 }
-            }
-        };     
+            };
         } else {
-           window.HW_config = {
-            account: options.id,
-            selector: options.location
-        };
-        }     
+            window.HW_config = {
+                account: options.id,
+                selector: '.eager-headway-app'
+            };
+        }
     };
-    var async = function(u, c) {
-        var d = document, t = 'script',
-          o = d.createElement(t),
-          s = d.getElementsByTagName(t)[0];
-        o.src = '//' + u;
-        if (c) { o.addEventListener('load', function (e) { c(null, e); }, false); }
-        s.parentNode.insertBefore(o, s);
+    var async = function(src, cb) {
+        var el = document.createElement('script');
+        el.src = '//' + src;
+
+        if (cb) {
+            el.addEventListener('load', function(e) {
+                cb(null, e);
+            });
+        }
+
+        document.head.appendChild(el);
     };
     var initApp = function() {
         render();
-    if (!window.addEventListener || !options.id) {
-        return;
-    } 
-    async('cdn.headwayapp.co/widget.js', function() {});
+
+        if (!options.id) {
+            return;
+        }
+
+        async('cdn.headwayapp.co/widget.js');
     };
-    var setOptions = function(opts) {
-        options = opts;
-        initApp();
-    };
+
     if (document.readyState === 'loading') {
         document.addEventListener('DOMContentLoaded', initApp);
-    }
-    else {
+    } else {
         initApp();
     }
-    INSTALL_SCOPE = {
-        setOptions: setOptions
-    };
 })();
